004ca8e549c9005605e1c78ddcc86811d167658e
“SterlikovSergey” <sterlikovsergey@icloud.com>
2023-03-22 22:02:14 +0300

    The lesson2 is finished

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,16 +1,39 @@
 import java.util.Scanner;
-//  public -  видят все
-//  static -
-//  void - не чего не возвращает
-//  main - start apply
-
-
 
 public class HelloWorld {
+    static int c;
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter your name please ");
-        String str = scanner.nextLine();
-        System.out.println(str + " Hello World");
+        System.out.println(c);
+        byte a = 3;
+        short myShort = a;  // byte -> short -> int -> long = true
+        int myInt = myShort;
+        long myLong = myInt;
+        String newString = String.valueOf(a);
+        char letter  = 2;
+        float myFloat = 3.434f;
+        double myDouble =  4.5555;
+        boolean  isTrue = true;
+        boolean isFalse = false;
+        System.out.println(newString + " side");
+
+        if(a + letter == 5){
+            System.out.println(isTrue);
+        } else {
+            System.out.println(isFalse);
+        }
+
+        System.out.println(isTrue);
+
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Enter your name please "); // command / - закоментировать выделенный  код
+//        String str = scanner.nextLine();
+//        System.out.println(str + " Hello World");
+
+
+        System.out.println((double) 4/3); // isTrue
+        System.out.println((double) (4/3)); // isFalse
+
+
+
     }
 }

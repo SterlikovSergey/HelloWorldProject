554c62d5240497bd96db93f87741b6da53fdf6d7
“SterlikovSergey” <sterlikovsergey@icloud.com>
2023-03-26 21:28:36 +0300

    Additional task

---
+++ src/HelloUser.java
@@ -0,0 +1,19 @@
+
+import java.util.Scanner;
+
+public class HelloUser {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+
+            System.out.println(" Please enter your name");
+            String user = scanner.nextLine();
+            System.out.println(" Ok! " + user + ", Welcome to Java ");
+
+        }
+
+    }
+}
+

a64908009f696e6c717f6f353acd8d29b16e458e
“SterlikovSergey” <sterlikovsergey@icloud.com>
2023-03-27 22:00:22 +0300

    finish

--- src/by/Sterlikov/lesson3/RandomExemple.java
+++ src/by/Sterlikov/lesson3/RandomExemple.java
@@ -4,10 +4,21 @@ import java.util.Random;
 
 public class RandomExemple {
     public static void main(String[] args) {
-        Random random = new Random();
+        int theNumber = (int) (Math.random() * 200 + 1);
 
-        for(int i = 0; i < 10; i++){
+
+
+
+
+        Random random = new Random();
+        for (int i = 0; i < 100; i+=5) {
             System.out.println(random.nextInt());
+
+
+
+
+
+
         }
     }
 }

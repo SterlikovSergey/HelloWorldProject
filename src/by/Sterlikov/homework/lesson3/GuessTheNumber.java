317841ab53689c8a8d6feec25e539b50f76adda2
“SterlikovSergey” <sterlikovsergey@icloud.com>
2023-03-31 06:59:52 +0300

    guess the number

--- src/by/Sterlikov/homework/lesson3/GuessTheNumber.java
+++ src/by/Sterlikov/homework/lesson3/GuessTheNumber.java
@@ -1,32 +1,53 @@
 package by.Sterlikov.homework.lesson3;
 import java.util.Scanner;
-
 public class GuessTheNumber {
-        public static void main(String[] args) {
-            Scanner scanner = new Scanner(System.in);
-            while (true){
-                int theNumber = (int) (Math.random() * 200 + 1);
-                int value = 0;
-                System.out.println("Write any value BETWEEN 0 AND 200");
-                int count = 0;
-                while (value != theNumber) {
-                    System.out.println("number of attempts - " + count);
-                    System.out.println("Please input value  ");
-                    value = scanner.nextInt();
-                    if (value > theNumber) {
-                        System.out.println("Computer want value: " + theNumber);
-                        System.out.println("enter a number less than : " + value);
-                    } else if (value < theNumber) {
-                        System.out.println("Computer want value: " + theNumber);
-                        System.out.println("enter more than: " + value);
-                    } else {
-                        System.out.println("you win!!!");
-                        return;
-                    }
-                    count ++;
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.println("Do you want to play this game? Y or N ?");
+            String valueString = scanner.next();
+            if (valueString.equalsIgnoreCase("y")) {
+                int theNumber = (int) (Math.random() * 100 + 1);
+                System.out.println("Set the number of attempts! ");
+                int count = scanner.nextInt();
+                System.out.println("Please input value  ");
+                int value = scanner.nextInt();
+                if (value != theNumber) {
+                    //while (valueString.equalsIgnoreCase("y")) {
+                        System.out.println("number of attempts - " + count);
+                        if (count >= 1) {
+                            if (value > theNumber) {
+                                System.out.println("Computer want value: " + theNumber);
+                                System.out.println("enter a number less than : " + value);
+                                value = scanner.nextInt();
+                            } else if (value < theNumber) {
+                                System.out.println("Computer want value: " + theNumber);
+                                System.out.println("enter more than: " + value);
+                                value = scanner.nextInt();
+                            } else {
+                                System.out.println("You win!!!");
+                                System.out.println(" Do you want to play again? Y or N");
+                                valueString = scanner.next();
+                            }
+                            count--;
+                        } else {
+                            System.out.println("You lose !!! Number of attemptsYou lose !!! Number of attempts - " + count);
+                            System.out.println(" Do you want to play again? Y or N");
+                            valueString = scanner.next();
+                        }
+                    //}
+                } else {
+                    System.out.println("You win!!!");
+                    System.out.println(" Do you want to play again? Y or N");
+                    valueString = scanner.next();
                 }
+
+            } else {
+                System.out.println(" Goodbye !!!");
+                return;
             }
         }
+    }
 }
 
 

dcfb2fafd6eddf060f1584dae11988dfd185645c
SterlikovSergey <sergebyminsk@icloud.com>
2023-04-05 01:02:30 +0300

    Homework lesson 5 complete

---
+++ src/by/sterlikov/homework/lesson5/ChessBoardGit.java
@@ -0,0 +1,26 @@
+package by.sterlikov.homework.lesson5;
+
+public class ChessBoardGit {
+    public static void main(String[] args) {
+        //int [][] chessBoard = new int[8][8];
+        String [][] chessBoard= new String[8][8];
+        for (int i = 0; i < chessBoard.length;i++){
+            for(int j = 0; j < chessBoard[i].length;j++){
+                if((i+j)%2==0){
+                    //chessBoard[i][j] += 8;
+                    chessBoard[i][j] += "W";
+                } else {
+                    //chessBoard[i][j] += 0;
+                    chessBoard[i][j] += "B";
+                }
+            }
+        }
+        for (int i = 0; i < chessBoard.length;i++){
+            for(int j = 0; j < chessBoard[i].length;j++) {
+                System.out.print(chessBoard[i][j]);
+            }
+            System.out.println();
+        }
+    }
+}
+
---
+++ src/by/sterlikov/homework/lesson5/MultiplicationOfTwoMatricesGit.java
@@ -0,0 +1,28 @@
+package by.sterlikov.homework.lesson5;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class MultiplicationOfTwoMatricesGit {
+    public static void main(String[] args) {
+        Random random = new Random();
+        int [][] multiArrayOne = new int[3][3];
+        int [][] multiArrayTwo = new int[3][3];
+        int [][] multiArrayThree = new int[3][3];
+        int [][] multiArrayFour = new int[3][3];
+
+        for (int i = 0; i < multiArrayOne.length; i++){
+            for (int j = 0; j < multiArrayOne[i].length; j++){
+                multiArrayOne[i][j] = random.nextInt(5);
+                multiArrayTwo[i][j] = random.nextInt(5);
+                multiArrayThree[i][j] = multiArrayOne[i][j] * multiArrayTwo[i][j];
+                multiArrayFour[i][j] = multiArrayOne[i][j] +  multiArrayTwo[i][j];
+            }
+        }
+
+        System.out.println(Arrays.deepToString(multiArrayOne));
+        System.out.println(Arrays.deepToString(multiArrayTwo));
+        System.out.println(Arrays.deepToString(multiArrayThree));
+        System.out.println(Arrays.deepToString(multiArrayFour));
+    }
+}

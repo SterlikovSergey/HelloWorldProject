15b413ece78b7e7b0368274f5745d981efc10ac3
SterlikovSergey <sergebyminsk@icloud.com>
2023-04-05 00:52:06 +0300

    Homework lesson 5 complete

---
+++ src/by/sterlikov/homework/lesson5/MultiArray.java
@@ -0,0 +1,38 @@
+package by.sterlikov.homework.lesson5;
+
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
+public class MultiArray {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+
+        int [][][] multiArray = new int[3][3][3];
+
+        for (int i = 0; i < multiArray.length; i++){
+            for (int j = 0; j < multiArray[i].length; j++){
+                for (int k = 0; k < multiArray[i][j].length; k++){
+                    multiArray[i][j][k] = random.nextInt(5);
+                }
+            }
+        }
+        System.out.println("Source array: ");
+        System.out.println(Arrays.deepToString(multiArray));
+        System.out.println("Enter value - ");
+
+        int count = scanner.nextInt();
+
+        for (int i = 0; i < multiArray.length; i++){
+            for (int j = 0; j < multiArray[i].length; j++){
+                for (int k = 0; k < multiArray[i][j].length; k++){
+                    multiArray[i][j][k] += count;
+                }
+            }
+        }
+        System.out.println("Resulting array: ");
+        System.out.println(Arrays.deepToString(multiArray));
+    }
+}

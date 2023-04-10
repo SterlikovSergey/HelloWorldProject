c99dd5d76b7fe47a60d0017a4e76b33ff59913b3
sergeysterlikov <sergebyminsk@icloud.com>
2023-04-10 19:35:27 +0300

    homework lesson6 CreditCard intro

---
+++ src/by/sterlikov/homework/lesson6/AdditionalTask2/ATM.java
@@ -0,0 +1,54 @@
+package by.sterlikov.homework.lesson6.AdditionalTask2;
+
+public class ATM {
+
+    private int countOfBanknotes20 = 0;
+    private int countOfBanknotes50 = 0;
+    private int countOfBanknotes100;
+    private int moneyOnBalance;
+
+    public ATM() {
+    }
+
+    public ATM(int countOfBanknotes20) {
+        this.countOfBanknotes20 = countOfBanknotes20;
+    }
+
+    public ATM(int countOfBanknotes20, int countOfBanknotes50) {
+        this.countOfBanknotes20 = countOfBanknotes20;
+        this.countOfBanknotes50 = countOfBanknotes50;
+    }
+
+    public ATM(int countOfBanknotes20, int countOfBanknotes50, int countOfBanknotes100) {
+        this.countOfBanknotes20 = countOfBanknotes20;
+        this.countOfBanknotes50 = countOfBanknotes50;
+        this.countOfBanknotes100 = countOfBanknotes100;
+    }
+
+    public void addMoney(int money){
+        if(money == 20){
+            countOfBanknotes20 += 1;
+            moneyOnBalance += money;
+            System.out.println("Credit to your balance - " + money + " rub.");
+        } if(money == 50){
+            countOfBanknotes50 +=1;
+            moneyOnBalance += money;
+            System.out.println("Credit to your balance - " + money + " rub.");
+        } if(money == 100){
+            countOfBanknotes100 +=1;
+            moneyOnBalance += money;
+            System.out.println("Credit to your balance - " + money + " rub.");
+        } else  {
+            System.out.println("Banknotes accepted only in denominations of 20, 50, 100");
+        }
+    }
+
+    public boolean removedMoney (int money){
+        if (moneyOnBalance > 0){
+            moneyOnBalance -= money;
+        } else {
+            System.out.println("No money");
+        }
+        return true;
+    }
+}

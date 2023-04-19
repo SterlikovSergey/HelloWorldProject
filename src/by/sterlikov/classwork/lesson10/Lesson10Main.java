f87a446cf3aee79eb5d3336acbceb91441ee2178
sergeysterlikov <sergebyminsk@icloud.com>
2023-04-19 23:56:55 +0300

    classwork/lesson10 completed

---
+++ src/by/sterlikov/classwork/lesson10/Lesson10Main.java
@@ -0,0 +1,43 @@
+package by.sterlikov.classwork.lesson10;
+
+public class Lesson10Main {
+    public static void main(String[] args) {
+        String str = "This is my string";
+        String str2 =  "This is my string";
+        String str3 = new String("This is my string");
+        System.out.println(str2 == str); // так сравнивать строки нельзя!!!
+        System.out.println(str == str3);
+
+        System.out.println(str.equals(str2));//  для сравнения занчения в строке
+
+        String addString = str + str2;
+        System.out.println(addString);
+        System.out.println(str.concat(str2));
+        String [] arr = new String[]{str2, str, str3};
+        System.out.println(String.join(". ", str2, str, str3));// использовать для обьеденения строк
+        System.out.println(String.join(". ", arr));
+
+        String myBestString = "      This is my best string (space)    ";
+        System.out.println(myBestString = myBestString.trim());// уберает пробелы в начале и в конце строки
+        myBestString = myBestString.trim();
+        System.out.println(myBestString.length());
+
+        System.out.println(myBestString.charAt(5)); // взять сисвол в строке.
+
+        for (int i = 0; i < myBestString.length(); i++){
+            System.out.print(myBestString.charAt(i));
+        }
+        char[] charArray = myBestString.toCharArray();
+        int count = 0;
+        for (char ch: charArray){
+            if(ch == 'i'){
+                count++;
+            }
+        }
+        System.out.println("Count " + count);
+
+        System.out.println(myBestString.substring(4)); // возвращает строку с указанной точки отсчёта
+        System.out.println(myBestString.substring(4,10)); //  в выбранном диапазоне
+
+    }
+}
---
+++ src/by/sterlikov/classwork/lesson10/Lesson10Main2.java
@@ -0,0 +1,48 @@
+package by.sterlikov.classwork.lesson10;
+
+import java.util.Arrays;
+
+public class Lesson10Main2 {
+    public static void main(String[] args) {
+
+        String str = "This is my string";
+        String str2 = "THIS IS MY STRING";
+
+        System.out.println(str.toLowerCase());
+        System.out.println(str.toUpperCase());
+        System.out.println(str.equalsIgnoreCase(str2));
+
+        System.out.println(str.replace('i', 'a'));
+        System.out.println(str.replace("is", "as"));
+
+        System.out.println(str.concat("is"));
+        System.out.println(str.indexOf("is")); // вернет номер индекса  с начала строки
+        System.out.println(str.lastIndexOf("is"));
+        System.out.println(str.indexOf("is", 15)); //если вернул  отрицательное значение, значит не нашёл!
+
+        System.out.println(str.replaceFirst("","a"));  // замена regex  на другое в replacement
+        System.out.println(str.replaceAll("","a"));    // замена regex  на другое в replacement
+
+        String str4 = "abra";
+        System.out.println(str4.repeat(5));    // склеить строку n раз
+        System.out.println(str4.startsWith("ab"));
+        System.out.println(str4.endsWith("ab"));
+
+        String[] myArrayString = str.split(" "); // разбить строку на массив слов, разделённых regex
+        System.out.println(Arrays.toString(myArrayString)); // между словами не более одного пробела
+
+        String str5 = " ";
+        System.out.println(str5.isEmpty()); // пустая строчка ?
+        System.out.println(str5.isBlank()); // чистая строка ?
+
+        int myValue = 125;
+        String result = String.valueOf(myValue); // точно  одна строка !!!
+        String result2 = 125 + ""; // неизвестно сколько сторок  в значении result2 !!!
+        System.out.println(result + " " + result2);
+
+
+
+
+
+    }
+}

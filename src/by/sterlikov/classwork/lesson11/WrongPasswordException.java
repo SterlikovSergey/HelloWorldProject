596af7acbe9a2f50f917483d608288171397f5ad
sergeysterlikov <sterlikovsergey@icloud.com>
2023-05-01 00:38:20 +0300

    Additional task homework lesson 11 completed

--- src/by/sterlikov/classwork/lesson11/WrongPasswordException.java
+++ src/by/sterlikov/classwork/lesson11/WrongPasswordException.java
@@ -1,6 +1,9 @@
 package by.sterlikov.classwork.lesson11;
 
 public class WrongPasswordException extends Exception{
+    public WrongPasswordException(){
+
+    }
     public WrongPasswordException(String message){
         super(message);
     }
--- src/by/sterlikov/extrawork3/MyBestLogin.java
+++ src/by/sterlikov/extrawork3/MyBestLogin.java
@@ -12,23 +12,21 @@ public class MyBestLogin extends JFrame {
     private JTextField loginField1;
     private JPasswordField passwordField;
     private JButton button1;
+    private JPasswordField passwordField1;
 
     public MyBestLogin() {
         button1.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 try {
-                    checkCredential(loginField1.getText(), passwordField.getText());
-                    JOptionPane.showMessageDialog(MyBestLogin.this, "Login and password are correct");
+                    checkCredential(loginField1.getText(), passwordField.getText(), passwordField1.getText());
+                    JOptionPane.showMessageDialog(MyBestLogin.this, "All are correct");
                 } catch (WrongLoginException ex) {
-                    JOptionPane.showMessageDialog(MyBestLogin.this, "Oops something bad with login");
-
+                    JOptionPane.showMessageDialog(MyBestLogin.this, ex.getMessage());
                 } catch (WrongPasswordException ex) {
-                    JOptionPane.showMessageDialog(MyBestLogin.this, "Oops something bad with password");
-
+                    JOptionPane.showMessageDialog(MyBestLogin.this, ex.getMessage());
                 } catch (Exception ex) {
-                    JOptionPane.showMessageDialog(MyBestLogin.this, "Access denied");
-
+                    JOptionPane.showMessageDialog(MyBestLogin.this, ex.getMessage());
                 }
             }
         });
@@ -38,24 +36,23 @@ public class MyBestLogin extends JFrame {
         MyBestLogin form = new MyBestLogin();
         form.setTitle("THIS IS MY APP");
         form.setContentPane(form.panel);
-        form.setSize(250, 120);
+        form.setSize(250, 200);
         form.setResizable(false);
         form.setLocation(500, 300);
         form.setDefaultCloseOperation(EXIT_ON_CLOSE);
         form.setVisible(true);
     }
 
-    private static boolean checkCredential(String login, String password) throws Exception {
-        String correctlogin = "Super";
-        String correctPassword = "Puper";
-
-        if (login.length() < 3) throw new WrongLoginException("Login is short");
-
-        if (password.length() < 3) throw new WrongPasswordException("Password is short");
-
-        if (login.equals(correctlogin) && password.equals(correctPassword)) ;
-
-        throw new Exception("Login or password are incorrect!");
-
+    private static boolean checkCredential(String login, String password, String confirmPassword) throws Exception {
+        if (login.length() > 20) throw new WrongLoginException("login must not be longer than 20 characters");
+        if (login.contains(" ")) throw new WrongLoginException("login must not contain a space");
+        if (password.length() > 20) throw new WrongPasswordException("Password must not be longer than 20 characters");
+        if (password.contains(" ")) throw new WrongPasswordException("Password must not contain a space");
+        if ((password.codePoints().filter(Character::isDigit).count() < 1))
+            throw new WrongPasswordException("Password must contain numbers");
+        if (confirmPassword.equals(password)) {
+            return true;
+        }
+        throw new Exception("Passwords do not match");
     }
 }

5bca63408c53fc41a65ce39d6d64f044d49dc176
sergeysterlikov <sterlikovsergey@icloud.com>
2023-05-31 19:15:32 +0300

    homeworklesson16 completed 1,2,3,4 tasks

--- src/by/sterlikov/homework/lesson16/MainExample.java
+++ src/by/sterlikov/homework/lesson16/MainExample.java
@@ -1,36 +1,65 @@
 package by.sterlikov.homework.lesson16;
 
-import java.awt.*;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
+import java.util.function.Function;
+
+import static java.util.stream.Collectors.counting;
+import static java.util.stream.Collectors.groupingBy;
 
 public class MainExample {
     public static void main(String[] args) {
         List<Book> books = getBooks();
 
-        Map<Integer, List<Book>> booksByYear = books
-                .stream()
-                .collect(Collectors.groupingBy(Book::getYearPublishing));
-        System.out.println(booksByYear);
+        printGroupingByYear(books);
+        printGroupingByAuthorByYear(books);
+        printCountingBooksByYear(books);
+        numberBooksByAuthor(books);
 
-/*        Map<Integer,List<Book>> bookByNameTitle = books
-                .stream()
-                .flatMap(book -> book
-                        .g
-                        )*/
 
     }
 
+   private static void numberBooksByAuthor(List<Book> books) {
+        Map<String,Long> numberBooksByAuthor = books
+                .stream()
+                .collect(groupingBy(Book::getAuthor,
+                        counting()));
+        System.out.println(numberBooksByAuthor);
+    }
+
+    private static void printCountingBooksByYear(List<Book> books) {
+        Map<Integer, Long> printCountingBooksByYear = books
+                .stream()
+                .collect(groupingBy(Book::getYearPublishing
+                ,counting()));
+        System.out.println("Counting" + printCountingBooksByYear);
+    }
+
+    private static void printGroupingByYear(List<Book> books) {
+        Map<Integer, List<Book>> printGroupingByYear = books
+                .stream()
+                .collect(groupingBy(Book::getYearPublishing));
+        System.out.println("GroupingByYear" + printGroupingByYear);
+    }
+
+    private static void printGroupingByAuthorByYear(List<Book> books) {
+        Map<String, Map<Integer, List<Book>>> bookByNameTitleByYear = books
+                .stream()
+                .collect(groupingBy(Book::getAuthor, groupingBy(Book::getYearPublishing)));
+        System.out.println("GroupingBYTitle" + bookByNameTitleByYear);
+    }
+
     private static List<Book> getBooks() {
-        List<Book> books = Arrays.asList(new Book("George", "War",2023),
-                new Book("Bon", "Life",2022),
+        List<Book> books = Arrays.asList(new Book("George", "War", 2023),
+                new Book("George", "Pepsi", 2022),
+                new Book("Bon", "Life", 2022),
                 new Book("Rudolf", "Barny", 2023),
-                new Book("Piter", "Pan",2020),
-                new Book("Kristofer","Terminator",2020),
-                new Book("Mel","Gibson",2020));
+                new Book("Piter", "Pan", 2020),
+                new Book("Kristofer", "Terminator", 2020),
+                new Book("Bon", "Dead", 2021),
+                new Book("Mel", "Gibson", 2020));
+
         return books;
     }
 }
